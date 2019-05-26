d8b3712d173265ad220d2558aa5d392c8bec4e02
pcnimdock <acaballoregaladonolemires@gmail.com>
2019-05-26 18:06:34 +0200

    Add files via upload

---
+++ firmware/Src/main.c
@@ -0,0 +1,408 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "usb_device.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart3;
+DMA_HandleTypeDef hdma_usart3_rx;
+DMA_HandleTypeDef hdma_usart3_tx;
+
+/* USER CODE BEGIN PV */
+extern uint8_t uart3_timeout_error;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
+static void MX_USART3_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+#define JOYSTICK_REPORT_SIZE 8
+uint8_t joystick[JOYSTICK_REPORT_SIZE]={0xFF,0xFE,0,0,0,0,0,0};
+uint8_t ms;
+uint8_t sending=0;cmd_encola=0;
+extern USBD_HandleTypeDef hUsbDeviceFS;
+uint16_t offsets_mode[3]={0};
+uint8_t next_point=0;
+
+uint8_t data_recived=0;
+
+#define BUTTON_TRIGGER			0x2000
+#define BUTTON_A						0x0008
+#define BUTTON_B						0x0004
+#define BUTTON_C						0x0002
+#define BUTTON_SELECT				0x4000
+#define BUTTON_START				0x8000
+#define DPAD_UP      				0x0010
+#define DPAD_DOWN						0x0040
+#define DPAD_LEFT 					0x0080
+#define DPAD_RIGHT 0x0020
+
+#define RX_BUFF_SIZE 10
+
+
+
+extern uint8_t next_point;
+extern uint16_t offsets_mode[3];
+uint16_t x=120,y=32767-120;
+uint16_t btn;
+uint8_t progressive=0;
+
+uint8_t RX_BUFF[11];
+
+uint8_t cad_init[]="Iniciado... \r\n";
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+static uint8_t cad_recv[]="recibido\n";
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+uint16_t data_to_send;
+uint8_t send_cad[255];
+  /* USER CODE END 1 */
+  
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_USART3_UART_Init();
+  MX_USB_DEVICE_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_UART_Transmit_DMA(&huart3, cad_init, 14);
+  HAL_UART_Receive_DMA(&huart3, (uint8_t *)RX_BUFF, 7);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	while (1) {
+		/* USER CODE END WHILE */
+
+		/* USER CODE BEGIN 3 */
+
+		if (uart3_timeout_error) {
+			//reiniciar recepción DMA
+
+		//	HAL_UART_Receive_DMA(&huart3, (uint8_t *) RX_BUFF, 8);
+			uart3_timeout_error = 0;
+		}
+
+		if(data_recived)
+		{
+			joystick[0]=RX_BUFF[0];
+			joystick[1]=RX_BUFF[1];
+			joystick[2]=RX_BUFF[2];
+			joystick[3]=RX_BUFF[3];
+			joystick[4]=RX_BUFF[4];
+			joystick[5]=RX_BUFF[5];
+			ms = RX_BUFF[6];
+			btn=((uint16_t)(RX_BUFF[0]))*256;
+			btn|=RX_BUFF[1];
+			data_recived=0;
+			if(next_point>0)
+			{next_point=0;
+			data_to_send=sprintf(send_cad,"conf: %x %x %x\r\n",offsets_mode[0],offsets_mode[1],offsets_mode[2]);
+			if(offsets_mode[2]!=0)
+			{progressive=1;}
+			}
+			else
+			{
+				if(progressive)
+							{
+								joystick[0]|=0x01;
+							}
+				data_to_send=sprintf(send_cad,"echo: %x %x %x %x %x %x\r\n",joystick[0],joystick[1],joystick[2],joystick[3],joystick[4],joystick[5]);
+			}
+			uint8_t temp_pin;
+						temp_pin=HAL_GPIO_ReadPin(BTN_WK_GPIO_Port, BTN_WK_Pin);
+			HAL_UART_Transmit_DMA(&huart3, send_cad, data_to_send);
+			USBD_HID_SendReport(&hUsbDeviceFS, joystick, JOYSTICK_REPORT_SIZE);
+
+			if((temp_pin==1)&&((btn&BUTTON_TRIGGER)==0))
+			{
+				//esto es modo calibración
+				//se debe mandar después de 8-9ms una cadena de reinicio
+				uint8_t temp;
+				temp=joystick[1];
+				joystick[2]=joystick[3]=joystick[4]=joystick[5]=0;
+				joystick[0]=0xFE;//joystick[1]=0xFF;
+				if(progressive){joystick[0]=0xFF;}
+				HAL_Delay(9);
+				USBD_HID_SendReport(&hUsbDeviceFS, joystick, JOYSTICK_REPORT_SIZE);
+//				joystick[2]=joystick[3]=joystick[4]=joystick[5]=0;
+//								joystick[0]=0xFE;joystick[1]=0xFF;
+//								if(progressive){joystick[0]=0xFF;}
+//								HAL_Delay(9);
+//								USBD_HID_SendReport(&hUsbDeviceFS, joystick, JOYSTICK_REPORT_SIZE);
+			}
+		}
+	/*	joystick[2] = x & 0xFF;
+		joystick[3] = x / 256;
+		joystick[4] = y & 0xFF;
+		joystick[5] = y / 256;
+		joystick[6] = 0;
+		joystick[7] = 0;*/
+//		if (x > 700) {
+//			x = 120;
+//		}
+//		if (y > 32767 + 120) {
+//			y = 32767 - 120;
+//		}
+	}
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage 
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 6;
+  RCC_OscInitStruct.PLL.PLLN = 72;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 3;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART3_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART3_Init 0 */
+
+  /* USER CODE END USART3_Init 0 */
+
+  /* USER CODE BEGIN USART3_Init 1 */
+
+  /* USER CODE END USART3_Init 1 */
+  huart3.Instance = USART3;
+  huart3.Init.BaudRate = 115200;
+  huart3.Init.WordLength = UART_WORDLENGTH_8B;
+  huart3.Init.StopBits = UART_STOPBITS_1;
+  huart3.Init.Parity = UART_PARITY_NONE;
+  huart3.Init.Mode = UART_MODE_TX_RX;
+  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART3_Init 2 */
+  __HAL_UART_ENABLE_IT(&huart3,UART_IT_IDLE);
+  /* USER CODE END USART3_Init 2 */
+
+}
+
+/** 
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void) 
+{
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Stream1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Stream1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Stream1_IRQn);
+  /* DMA1_Stream3_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Stream3_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Stream3_IRQn);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : LED_Pin */
+  GPIO_InitStruct.Pin = LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : BTN_WK_Pin */
+  GPIO_InitStruct.Pin = BTN_WK_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(BTN_WK_GPIO_Port, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+/**
+  * @brief  Tx Transfer completed callback
+  * @param  huart: UART handle.
+  * @note   This example shows a simple way to report end of DMA Tx transfer, and
+  *         you can add your own implementation.
+  * @retval None
+  */
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
+{
+  /* Turn LED1 on: Transfer in transmission process is correct */
+
+}
+
+/**
+  * @brief  Rx Transfer completed callback
+  * @param  huart: UART handle
+  * @note   This example shows a simple way to report end of DMA Rx transfer, and
+  *         you can add your own implementation.
+  * @retval None
+  */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+  /* Turn LED2 on: Transfer in reception process is correct */
+
+	data_recived=1;
+}
+
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
